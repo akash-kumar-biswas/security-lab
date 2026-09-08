256ef8b3f02ee6cfab329c908a70f4ca2726f5a0
akash-kumar-biswas <akash.biswas.cse@gmail.com>
2026-09-09 00:48:03 +0600

    Elgamal

--- elgamal-rerandomization.cpp
+++ elgamal-rerandomization.cpp
@@ -3,68 +3,38 @@ using namespace std;
 
 using ll = long long;
 
-ll modpow(ll a, ll e, ll m){
-    ll r = 1;
-    a %= m;
-
-    while (e){
-        if(e & 1){
-            r = (r*a) % m;
-        }
-        a = (a*a) % m;
-        e >>= 1;
-    }
-    return r;
-}
-
-ll egcd(ll a, ll b, ll &x, ll &y){
-    if(b == 0){
-        x = 1;
-        y = 0;
-        return a;
-    }
-
-    ll x1, y1;
-    ll g = egcd(b, a%b, x1, y1);
-    x = y1;
-    y = x1 - (a/b)*y1;
-    return g;
-}
-
-ll modinv(ll a, ll m){
-    ll x, y;
-    ll g = egcd(a, m, x, y);
-    return (x%m + m) % m;
-}
 
 int main(){
-    ll alpha = 6, p = 79, x = 15, beta = modpow(alpha, x, p);
+    ll p = 79, g;
 
-    cout << "alpha , beta, p : " << alpha << " " << beta << " " << p << endl;
+    cout << "Enter g: " ;
+    cin >> g;
 
-    cout<< "x : " << x << endl;
+    vector<ll> residues;
 
-    ll m = 60, r1 = 7;
+    ll curr = 1;
 
-    cout << "m : " << m << endl;
+    for(ll i = 1; i < p; i++){
+        curr = (curr * g)% p;
+        residues.push_back(curr);
+    }
 
-    ll c1 = modpow(alpha, r1, p);
-    ll c2 = (m * modpow(beta, r1, p)) % p;
+    sort(residues.begin(), residues.end());
 
-    cout << "c1, c2 : " << c1 << " " << c2 << endl;
+    int f = 1;
+    for(ll i = 0; i < residues.size(); i++){
+        if(residues[i] != (i+1)){
+            f = 0;
+            break;
+        }
+    }
 
-    ll  r2 = 9;
+    if(f){
+        cout << "Valid";
+    }
+    else
+        cout << "Invalid";
 
-
-    ll c1_new = (c1* modpow(alpha, r2, p)) % p;
-    ll c2_new = (c2 * modpow(beta, r2, p)) % p;
-
-    cout << "c1_new, c2_new : " << c1_new << " " << c2_new << endl;
-  
-
-    ll m_dec = (c2_new * modinv(modpow(c1_new, x, p), p) % p);
-
-    cout << "m_dec : " << m_dec << endl;
 
     return 0;
 }
